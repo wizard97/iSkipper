d5d97ea982ccf1e4ae7e954884eb2aa1fbf09736
Aaron Wisner <aaronwisner@gmail.com>
2017-02-28 18:34:43 -0500

    Implemented beginRecv to put NRF radio into same mode as iClicker's

--- iClickerEmulator.cpp
+++ iClickerEmulator.cpp
@@ -6,9 +6,27 @@ iClickerEmulator::iClickerEmulator(uint8_t _cepin, uint8_t _cspin, uint8_t _irqp
     pinMode(_irqpin, OUTPUT);
 }
 
-bool iClickerEmulator::begin()
+bool iClickerEmulator::beginRecv()
 {
     _radio.begin();
-    _radio.setCRCLength(RF24_CRC_16 );
-    _radio.powerUp();
+    _radio.powerUp(); // Turn on
+    _radio.setCRCLength(RF24_CRC_16); //Enable 16 bit crc
+    _radio.maskIRQ(1, 1, 0); //Mask all interrupts except RX_DR
+    _radio.setPayloadSize(PACKET_SIZE); // Set packet size
+    _radio.setAddressWidth(3); // Set the mac address width
+    _radio.setChannel(CHANNEL); // Set to the correct channel
+    _radio.setPALevel(RF24_PA_MAX); // Max out power
+    _radio.seDataRate(RF24_1MBPS); // Set data rate 1mbps
+
+    _radio.openReadingPipe(1, clickerMasterMAC); // Open reading pipe on pipe 1
+    _radio.startListening(); // Start listening
+
+    // WTF is flush_rx private?!?!
+    tmp uint8_t[32];
+    while(_radio.availible())
+        _radio.read(tmp, sizeof(tmp));
+
+
+    return true;
+
 }
--- iClickerEmulator.h
+++ iClickerEmulator.h
@@ -4,10 +4,22 @@
 #include "RF24.h"
 #include "Arduino.h"
 
+
+#define MAC_SIZE 3
+#define DATA_SIZE 1
+#define PACKET_SIZE (MAC_SIZE + DATA_SIZE)
+
+#define CHANNEL 47
+
+
 class iClickerEmulator
 {
 public:
     iClickerEmulator(uint8_t _cepin, uint8_t _cspin, uint8_t _irqpin);
+
+    bool beginRecv();
+
+    const uint8_t clickerMasterMAC[MAC_SIZE] = { 0x56, 0x34, 0x12 };
 
 private:
     RF24 _radio;
