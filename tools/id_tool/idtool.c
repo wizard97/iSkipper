ebaefa8270171c41ccb47b185e23c942b1696513
Aaron Wisner <aaronwisner@gmail.com>
2017-03-12 15:49:13 -0400

    Rearranged folder, added ability to generate id

--- tools/id_tool/idtool.c
+++ tools/id_tool/idtool.c
@@ -1,10 +1,11 @@
 // This is just a test of reversing an iclicker id
-// compile with gcc idhash.c -o idhash
+// compile with gcc idtool.c -o idtool
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>       /* time */
 
 #define KNRM  "\x1B[0m"
 #define KRED  "\x1B[31m"
@@ -18,38 +19,68 @@
 void encode(uint8_t *addr, uint8_t *ret);
 void decode(uint8_t *addr, uint8_t *ret);
 bool valid_id(uint8_t addr[4]);
+void generate_id(uint8_t addr[4]);
+void print_help();
+
 
 int main(int argc, char **argv)
 {
-    if (argc != 3) {
-        printf("Ex (Original id to transmit hash):\n./idhash -o 0x8F941803\n./idhash -f 0x12345678\n");
+    if (argc != 3 && argc != 2) {
+        print_help();
         exit(1);
     }
+    srand(time(NULL));
 
-    uint32_t input;
+
     uint8_t tmp[4];
-    sscanf(argv[2], "%x", &input);
 
-    uint8_t *inb = (uint8_t *)&input;
-    uint8_t addr[] = { inb[3], inb[2], inb[1], inb[0] };
-
-
-    printf("%sInput: %#x %#x %#x %#x\n", KBLU, addr[0], addr[1], addr[2], addr[3]);
 
     if (!strcmp(argv[1], "-o")) {
+        uint32_t input;
+        sscanf(argv[2], "%x", &input);
+        uint8_t *inb = (uint8_t *)&input;
+        uint8_t addr[] = { inb[3], inb[2], inb[1], inb[0] };
+
+        printf("%sInput: %#x %#x %#x %#x\n", KBLU, addr[0], addr[1], addr[2], addr[3]);
+
         if (!valid_id(addr))
             printf("%sNOTE: THIS IS NOT A VALID ICLICKER ID!!\n", KRED);
         encode(addr, tmp);
         printf("%sEncoded: %#x %#x %#x %#x\n",KGRN, tmp[0], tmp[1], tmp[2], tmp[3]);
     } else if (!strcmp(argv[1], "-f")) {
+        uint32_t input;
+        sscanf(argv[2], "%x", &input);
+        uint8_t *inb = (uint8_t *)&input;
+        uint8_t addr[] = { inb[3], inb[2], inb[1], inb[0] };
+
+        printf("%sInput: %#x %#x %#x %#x\n", KBLU, addr[0], addr[1], addr[2], addr[3]);
+        
         decode(addr, tmp);
         printf("%sDecoded: %#x %#x %#x %#x\n", KGRN, tmp[0], tmp[1], tmp[2], tmp[3]);
+    } else if(!strcmp(argv[1], "-g")) {
+        generate_id(tmp);
+        printf("%sRandom ID: %#x %#x %#x %#x\n", KGRN, tmp[0], tmp[1], tmp[2], tmp[3]);
+    } else {
+        print_help();
     }
 
     printf("%s", KNRM);
     return 0;
 }
 
+
+void print_help()
+{
+    printf("-o: iClicker id to encoded id\n"
+            "-f: Encoded id to iClicker id\n"
+            "-g: Generate random iClicker ID\n"
+            "Examples:\n"
+            "./idtool -o 0x8F941803\n"
+            "./idtool -f 0x12345678\n"
+            "./idtool -g\n"
+        );
+
+}
 
 
 void encode(uint8_t *addr, uint8_t *ret)
@@ -73,4 +104,12 @@ void decode(uint8_t *addr, uint8_t *ret)
 bool valid_id(uint8_t addr[4])
 {
     return (addr[0]^addr[1]^addr[2]) == addr[3];
+}
+
+void generate_id(uint8_t addr[4])
+{
+    addr[0] = rand()%256;
+    addr[1] = rand()%256;
+    addr[2] = rand()%256;
+    addr[3] = addr[0]^addr[1]^addr[2];
 }
