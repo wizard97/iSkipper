eb5a041ca3f8393189fe3b6f0036a9557b2dd7e8
Aaron Wisner <daw268@cornell.edu>
2017-04-03 02:31:04 -0400

    Removed macros

--- emulator/iSkipper/iClickerRadio.h
+++ emulator/iSkipper/iClickerRadio.h
@@ -4,46 +4,6 @@
 #include <RingBufCPP.h>
 #include <Arduino.h>
 
-#define RF69_MAX_DATA_LEN       61 // to take advantage of the built in AES/CRC we want to limit the frame size to the internal FIFO size (66 bytes - 3 bytes overhead - 2 bytes crc)
-#define RF69_SPI_CS             SS // SS is the SPI slave select pin, for instance D10 on ATmega328
-
-// INT0 on AVRs should be connected to RFM69's DIO0 (ex on ATmega328 it's D2, on ATmega644/1284 it's D2)
-#if defined(__AVR_ATmega168__) || defined(__AVR_ATmega328P__) || defined(__AVR_ATmega88) || defined(__AVR_ATmega8__) || defined(__AVR_ATmega88__)
-  #define RF69_IRQ_PIN          2
-  #define RF69_IRQ_NUM          0
-#elif defined(__AVR_ATmega644P__) || defined(__AVR_ATmega1284P__)
-  #define RF69_IRQ_PIN          2
-  #define RF69_IRQ_NUM          2
-#elif defined(__AVR_ATmega32U4__)
-  #define RF69_IRQ_PIN          3
-  #define RF69_IRQ_NUM          0
-#elif defined(__arm__)//Use pin 10 or any pin you want
-  #define RF69_IRQ_PIN          10
-  #define RF69_IRQ_NUM          10
-#else
-  #define RF69_IRQ_PIN          2
-  #define RF69_IRQ_NUM          0
-#endif
-
-
-#define CSMA_LIMIT              -90 // upper RX signal sensitivity threshold in dBm for carrier sense access
-#define RF69_MODE_SLEEP         0 // XTAL OFF
-#define RF69_MODE_STANDBY       1 // XTAL ON
-#define RF69_MODE_SYNTH         2 // PLL ON
-#define RF69_MODE_RX            3 // RX MODE
-#define RF69_MODE_TX            4 // TX MODE
-
-// available frequency bands
-#define RF69_915MHZ            91
-
-#define null                  0
-#define COURSE_TEMP_COEF    -90 // puts the temperature reading in the ballpark, user can fine tune the returned value
-#define RF69_BROADCAST_ADDR 255
-#define RF69_CSMA_LIMIT_MS 1000
-#define RF69_TX_LIMIT_MS   1000
-#define RF69_FSTEP  61.03515625 // == FXOSC / 2^19 = 32MHz / 2^19 (p13 in datasheet)
-
-
 
 // Can buffer up to 20 packets
 #define PACKET_BUF_SIZE 20
